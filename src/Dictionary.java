cabdcc5ee612a01f0c6158a622bba0010fa159fd
Bryce Colson <brycec17@vt.edu>
2020-10-26 20:28:34 -0400

    Added Dictionary class

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -18,7 +18,7 @@ public class Dictionary {
 
     public void put(Word word) {
         scriptTrie.put(word.getScript(), word);
-        scriptTrie.put(word.getRomanization(), word);
+        romanizationTrie.put(word.getRomanization(), word);
         rootMap.put(word, word.getRoot());
         if (!derivativeMap.containsKey(word.getRoot())) {
             derivativeMap.put(word.getRoot(), new ArrayList<>());
@@ -26,7 +26,25 @@ public class Dictionary {
         derivativeMap.get(word.getRoot()).add(word);
     }
 
-    public List<Word> searchByScript(String str) {
+    public void putAll(List<Word> words) {
+        for (Word word : words) {
+            put(word);
+        }
+    }
 
+    public List<Word> searchByScript(String str) {
+        return scriptTrie.searchPartialMatches(str);
+    }
+
+    public List<Word> searchByRomanization(String str) {
+        return romanizationTrie.searchPartialMatches(str);
+    }
+
+    public String getRoot(Word word) {
+        return rootMap.get(word);
+    }
+
+    public List<Word> getDerivatives(String root) {
+        return derivativeMap.get(root);
     }
 }
